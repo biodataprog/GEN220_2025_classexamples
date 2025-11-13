58180e384e0b8086baa73110156114a923ffbe07
Jason Stajich <jasonstajich.phd@gmail.com>
2025-11-13 14:18:51 -0800

    add download

---
+++ Domains/download_cyano.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash -l
+curl -o Synechococcus_elongatus.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/022/984/195/GCF_022984195.1_ASM2298419v1/GCF_022984195.1_ASM2298419v1_protein.faa.gz
+curl -o Cylindrospermopsis_raciborskii.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/003/367/075/GCF_003367075.2_ASM336707v2/GCF_003367075.2_ASM336707v2_protein.faa.gz
+curl -o Cyanobium_gracile.faa.gz https://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/316/515/GCF_000316515.1_ASM31651v1/GCF_000316515.1_ASM31651v1_protein.faa.gz
+
